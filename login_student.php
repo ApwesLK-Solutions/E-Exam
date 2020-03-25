4cc18a9e10f10584ce9454a59890cd7fee4d5e69
Shehantha Edirisinghe <37338677+shehantha08@users.noreply.github.com>
2020-03-25 16:11:47 +0530

    Update login_student.php

--- login_student.php
+++ login_student.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="login_student.php"><b>E-Exam</b>Student</a>
+    <a href="login_student.php"><b>E-Exam</b> | Student</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
