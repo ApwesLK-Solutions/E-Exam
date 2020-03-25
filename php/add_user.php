273b32adbc871218b473a3d038cf126d367e3705
Chamod Priyamal <chamodpriyamal@zoho.com>
2020-03-25 14:51:56 +0530

    OTP Mechanism Installed

--- php/add_user.php
+++ php/add_user.php
@@ -2,27 +2,36 @@
 
     include 'database.php';
     
-    if(isset($_POST['name']) && isset($_POST['mobile']) && isset($_POST['password']) && isset($_POST['type']))
+    if(isset($_POST['name']) && isset($_POST['mobile']) && isset($_POST['password']) && isset($_POST['type']) && isset($_POST['otp']))
     {
-        $name = $_POST['name'];
-        $mobile = $_POST['mobile'];
-        $password = md5($_POST['password']);
-        $type = $_POST['type'];
-
-        $cmd = $conn->prepare("INSERT INTO users(name , mobile , password , type) VALUES(?,?,?,?)");
-        $cmd->bind_param("sssi",$name , $mobile , $password , $type);
-        if($cmd->execute())
+        session_start();
+        if(md5($_POST['otp']) == $_SESSION['hash'])
         {
-            echo "SUCCESS";
-            exit();
+            $name = $_POST['name'];
+            $mobile = $_POST['mobile'];
+            $password = md5($_POST['password']);
+            $type = $_POST['type'];
+
+            $cmd = $conn->prepare("INSERT INTO users(name , mobile , password , type) VALUES(?,?,?,?)");
+            $cmd->bind_param("sssi",$name , $mobile , $password , $type);
+            if($cmd->execute())
+            {
+                echo "SUCCESS";
+                exit();
+            }
+            else
+            {
+                if($cmd->errno == 1062)
+                {
+                    echo "ALRDEXI";
+                    exit();
+                }
+            }
         }
         else
         {
-            if($cmd->errno == 1062)
-            {
-                echo "ALRDEXI";
-                exit();
-            }
+            echo "OTPERR";
+            exit();
         }
     }
     else
@@ -39,3 +48,4 @@
     //POSTERR  -> POST REQUEST FAILED.
     //SUCCESS  -> USER ADD SUCCESS.
     //ALRDEXI  -> USER ALREADY EXISTS.
+    //OTPERR   -> OTP CODE WRONG.
--- php/otp.php
+++ php/otp.php
@@ -18,10 +18,14 @@
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, SITEURL . "php/sms.php");
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,"to=" . $to . "&text=" . $otp ."&ip=".$_SERVER['SERVER_ADDR']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"to=" . $to . "&text=Your E-Exam Verification Code : " . $otp ."&ip=".$_SERVER['SERVER_ADDR']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close ($ch);
     }
     else
     {
-
+        echo "ERROR";
+        exit();
     }
     
