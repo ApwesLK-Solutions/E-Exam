15b83a5a0d0ec0924e6029389329ad6bbece9cca
Shehantha Edirisinghe <37338677+shehantha08@users.noreply.github.com>
2020-03-26 20:23:24 +0530

    done

--- js/teacher_add_class.js
+++ js/teacher_add_class.js
@@ -3,29 +3,130 @@ function _(id)
     return document.getElementById(id);
 }
 
-//load grades to js
-function grade_load()
+document.body.onload = function()
 {
-    $('#grade').select2({
-        ajax: {
-            type: 'post',
-            url: '../php/get_grades.php',
-            dataType: 'json',
-            data: function () {
-                return {
-                    json: JSON.stringify(json),
-                    
-                    delay: 0.1
-                };
-            },
-            processResults: function (data) {
-                return {
-                    results: $.map(data, function(obj) {
-                        return { id: obj.ime, text: obj.ime };
-                    })
-                };
-            }
-        }
-    });
+    load_grades();
+    load_subjects();
+    load_institutes();
 }
 
+function load_grades()
+{
+    var request = new XMLHttpRequest();
+    request.open("POST","../php/get_grades.php",true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.onreadystatechange = function()
+    {
+        if(request.readyState == 4 && request.status == 200)
+        {
+            var selector = _('grade');
+            var result = JSON.parse(request.responseText);
+            for(i = 0 ; i < result.length ; ++i)
+            {
+                var opt = document.createElement('option');
+                opt.value = result[i].id;
+                opt.innerHTML = result[i].name;
+                selector.appendChild(opt);
+            }
+        }
+    }
+    request.send();
+}
+
+function load_subjects()
+{
+    var request = new XMLHttpRequest();
+    request.open("POST","../php/get_subjects.php",true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.onreadystatechange = function()
+    {
+        if(request.readyState == 4 && request.status == 200)
+        {
+            var selector = _('subject');
+            var result = JSON.parse(request.responseText);
+            for(i = 0 ; i < result.length ; ++i)
+            {
+                var opt = document.createElement('option');
+                opt.value = result[i].id;
+                opt.innerHTML = result[i].name;
+                selector.appendChild(opt);
+            }
+        }
+    }
+    request.send();
+}
+
+function load_institutes()
+{
+    var request = new XMLHttpRequest();
+    request.open("POST","../php/get_institutes.php",true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.onreadystatechange = function()
+    {
+        if(request.readyState == 4 && request.status == 200)
+        {
+            var selector = _('institute');
+            var result = JSON.parse(request.responseText);
+            for(i = 0 ; i < result.length ; ++i)
+            {
+                var opt = document.createElement('option');
+                opt.value = result[i].id;
+                opt.innerHTML = result[i].name;
+                selector.appendChild(opt);
+            }
+        }
+    }
+    request.send();
+}
+
+//Add class 
+document.forms.add_class.onsubmit = function(e)
+{
+    e.preventDefault();
+    if(_('grade').value == "" || _('subject').value == "" || _('institute').value == "" || _('discription').value == "" || _('enroll').value == "")
+    {
+        toastr.error("Please Fill All the Fields..");
+    }
+    else if(_("discription").value == "")
+    {
+        toastr.error("please enter discription about class and subject");
+    }
+    else if(_("enroll").value == "")
+    {
+        toastr.error("Please enter enrollment key");
+    }
+    else
+    {
+        var request = new XMLHttpRequest();
+        request.open("POST","php/add_class.php",true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.onreadystatechange = function()
+        {
+            if(request.readyState == 4 && request.status == 200)
+            {
+                var result = request.responseText;
+                if(result == "SUCCESS")
+                {
+                    toastr.success("New Class Has Been Added..");
+                }
+                else if(result == "FAILED")
+                {
+                    toastr.error("New Class Adding Failed..");
+                    
+                }
+                
+                else if(result == "POSTERR")
+                {
+                    toastr.error("Bad Request Contact Site Admin..");
+                    
+                }
+                else
+                {
+                    toastr.error("response was : " + request.responseText);
+                    grecaptcha.reset();
+                }
+            }
+        }
+        request.send("grade=" + _("grade").value + "&subject=" + _("subject").value + "&discription=" + _("discription").value + "&institute=" + _("institute").value + "&enroll=" + _("enroll").value); 
+    }
+}
