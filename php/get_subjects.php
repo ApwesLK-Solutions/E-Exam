829c90e82dd26abcb5436f590131b4dc4032aa9e
Chamod Priyamal <chamodpriyamal@zoho.com>
2020-03-26 19:12:29 +0530

    Grades Subjects Institute

---
+++ php/get_subjects.php
@@ -0,0 +1,22 @@
+<?php
+
+    include 'database.php';
+
+    $cmd = $conn->prepare("SELECT * FROM subject");
+    $cmd->execute();
+    $result = $cmd->get_result();
+    if($result->num_rows > 0)
+    {
+        $data =array();
+        while($row = $result->fetch_assoc())
+        {
+            array_push($data,$row);
+        }
+        echo json_encode($data);
+    }
+    else
+    {
+        echo "EMPTY";
+    }
+    
+    
