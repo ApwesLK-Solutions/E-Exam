f67d4e49cb65b463154b381513990f27f103ab74
Chamod Priyamal <chamodpriyamal@zoho.com>
2020-03-27 17:41:16 +0530

    fixed

--- js/teacher_my_class.js
+++ js/teacher_my_class.js
@@ -8,34 +8,39 @@ function update_visibility(e)
 {
     if(document.readyState == "complete")
     {
-        status = 1;
-    }
-    else
-    {
-        status = 0;
-    }
-    var request = new XMLHttpRequest();
-    request.open("POST","../php/teacher_update_class_enroll_key_visibility.php",true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    request.onreadystatechange = function()
-    {
-        if(request.readyState == 4 && request.status == 200)
+        var class_id = e.value;
+        var status;
+        if(e.checked)
         {
-            var result = request.responseText;
-            if(result == "SUCCESS")
+            status = 1;
+        }
+        else
+        {
+            status = 0;
+        }
+        var request = new XMLHttpRequest();
+        request.open("POST","../php/teacher_update_class_enroll_key_visibility.php",true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.onreadystatechange = function()
+        {
+            if(request.readyState == 4 && request.status == 200)
             {
-                toastr.success("You have been successfully changed enrollment key visibility..");
-            }
-            else if(result == "FAILED")
-            {
-                toastr.error("Enrollment key visibility change failed..");
-                if(status == 1)
+                var result = request.responseText;
+                if(result == "SUCCESS")
                 {
-                    e.checked = false;
+                    toastr.success("You have been successfully changed enrollment key visibility..");
                 }
-                else
+                else if(result == "FAILED")
                 {
-                    e.checked = true;
+                    toastr.error("Enrollment key visibility change failed..");
+                    if(status == 1)
+                    {
+                        e.checked = false;
+                    }
+                    else
+                    {
+                        e.checked = true;
+                    }
                 }
             }
         }
