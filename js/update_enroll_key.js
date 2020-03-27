c465286d879830fdc9f7f2f6b44a3f644dbd00ff
Shehantha Edirisinghe <37338677+shehantha08@users.noreply.github.com>
2020-03-27 13:43:41 +0530

    Create update_enroll_key.js

---
+++ js/update_enroll_key.js
@@ -0,0 +1,36 @@
+
+document.forms.add_class.onchange = function(e)
+{
+    e.preventDefault();
+
+        var ekeyvisible;
+        if(_('public').checked)
+        {
+            ekeyvisible = 1;
+        }
+        else
+        {
+            ekeyvisible = 0;
+        }
+        var request = new XMLHttpRequest();
+        request.open("POST","../php/teacher_update_class_enroll_key_visibility.php",true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.onreadystatechange = function()
+        {
+            if(request.readyState == 4 && request.status == 200)
+            {
+                var result = request.responseText;
+                if(result == "SUCCESS")
+                {
+                    toastr.success("You have been successfully changed enrollment ey visibility..");
+                }
+                else if(result == "FAILED")
+                {
+                    toastr.error("Enrollment key visibility change failed..");
+                    
+                }
+            }
+        }
+        request.send("ekeyvisible=" + ekeyvisible); 
+    
+}
